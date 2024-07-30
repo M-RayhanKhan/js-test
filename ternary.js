90b56279bf6506f0c65551365bd398996d4bf349
M-RayhanKhan <rayhansmartcode@gmail.com>
2024-07-30 10:25:59 +0600

    testing ternary operator

--- ternary.js
+++ ternary.js
@@ -1,4 +1,8 @@
 var myAge = 18;
 // ternary operator
 var type = (myAge >= 18) ? "adult" : (myAge <= 10) ? "child" : '000';
-console.log(type);
+// console.log(type);
+
+var isTrue = 5;
+var result = (isTrue >= 5);
+console.log(result);
